097ae182f945963c5c2649622a67adbd2632ddf2
John Wang <johnz.wang@outlook.com>
2024-05-07 21:36:36 +0200

    Flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+           nodejs
+           nodePackages.typescript-language-server
+        ];
+
+shellHook = ''
+        exec fish
+      '';
+      };
+    }
+  );
+}
